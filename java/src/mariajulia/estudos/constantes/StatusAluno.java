1a5cd38c10c89a891822a14780de3b0417be7df2
mariajuliasales <salesmariajulia11@gmail.com>
2023-01-09 23:22:42 -0300

    constante

---
+++ java/src/mariajulia/estudos/constantes/StatusAluno.java
@@ -0,0 +1,9 @@
+package mariajulia.estudos.constantes;
+
+public final class StatusAluno {
+
+	public static final String APROVADO = "Aprovado.";
+	public static final String RECUPERACAO = "Recuperação.";
+	public static final String REPROVADO = "Reprovado.";
+
+}
