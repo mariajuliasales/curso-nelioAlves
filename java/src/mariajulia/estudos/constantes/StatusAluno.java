5a80a91753245bc4bf16dd5a3431fe579be4ac36
mariajuliasales <salesmariajulia11@gmail.com>
2023-02-07 21:14:56 -0300

    modulo git e github

--- java/src/mariajulia/estudos/constantes/StatusAluno.java
+++
@@ -1,9 +0,0 @@
-package mariajulia.estudos.constantes;
-
-public final class StatusAluno {
-
-	public static final String APROVADO = "Aprovado.";
-	public static final String RECUPERACAO = "Recuperação.";
-	public static final String REPROVADO = "Reprovado.";
-
-}
